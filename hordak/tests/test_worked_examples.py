45b46dee85f76b5b800d223a34d27ae9aa9f439b
Adam Charnock <acharnock@twitter.com>
2016-09-12 19:03:54 +0100

    Creating worked test case for one month of a communal house billing system

--- hordak/tests/test_worked_examples.py
+++ hordak/tests/test_worked_examples.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.db import transaction as db_transaction
 
-from hordak.models import Account, Transaction, Leg
+from hordak.models import Account, Transaction, Leg, StatementImport, StatementLine
 
 
 class InitialEquityTestCase(TestCase):
@@ -143,3 +143,146 @@ class UtilityBillTestCase(TestCase):
         self.assertEqual(self.gas_payable.balance(), 0)
 
         Account.validate_accounting_equation()
+
+
+class CommunalHouseholdTestCase(TestCase):
+
+    def setUp(self):
+        T = Account.TYPES
+
+        self.bank = Account.objects.create(name='Bank', type=T.asset, code='0')
+
+        self.lia = Account.objects.create(name='Liabilities', type=T.liability, code='1')
+        # self.lia_deposits = Account.objects.create('Deposits', type=Account.TYPES.liability, code='1')
+        self.lia_elec_payable = Account.objects.create(name='Gas & Electricity Payable', parent=self.lia, code='2')
+        self.lia_rates_payable = Account.objects.create(name='Council Tax Payable', parent=self.lia, code='3')
+
+        self.inc = Account.objects.create(name='Income', type=T.income, code='2')
+        self.inc_housemate = Account.objects.create(name='Housemate Income', parent=self.inc, code='1')
+        self.inc_donation = Account.objects.create(name='Donation', parent=self.inc, code='2')
+
+        self.ex = Account.objects.create(name='Expenses', type=T.expense, code='3')
+        self.ex_rent = Account.objects.create(name='Rent', parent=self.ex, code='1')
+        self.ex_elec = Account.objects.create(name='Gas & Electricity', parent=self.ex, code='2')
+        self.ex_rates = Account.objects.create(name='Council Tax', parent=self.ex, code='3')
+        self.ex_food = Account.objects.create(name='Food', parent=self.ex, code='4')
+
+    def create_incoming_rent_payments(self, amount1, amount2):
+        statement_import = StatementImport.objects.create(bank_account=self.bank)
+        line1 = StatementLine.objects.create(date='2016-01-01', statement_import=statement_import, amount=amount1)
+        line2 = StatementLine.objects.create(date='2016-01-01', statement_import=statement_import, amount=amount2)
+        line1.refresh_from_db()
+        line2.refresh_from_db()
+        return (line1, line2)
+
+    def test_one_month(self):
+        """ Test payments over the course of three months
+
+        Costs are:
+
+          - 1000 rent per month (500/month/housemate)
+          - 120 electricity per 3 months, estimate (20/month/housemate, estimate)
+          - 180 rates per 3 months (30/month/housemate)
+          - 140 food per month, estimate in arrears (70/month/housemate, estimate)
+
+        Total: 620/month/housemate (with some estimation)
+
+        """
+        # Month 0 - We start by billing housemates for rent & gas & rates.
+        # (for now we have no 'bills', but assume housemates have paid).
+        # We have two housemates
+
+        # Firstly, before we even collect any money from housemates, we spend some on food
+        self.bank.transfer_to(self.ex_food, 35)
+        self.bank.transfer_to(self.ex_food, 35)
+        # Now we have negative money in the bank, and food expenses recorded
+        self.assertEqual(self.bank.balance(), -70)
+        self.assertEqual(self.ex_food.balance(), 70)
+
+        # Also, the landlord wants the rent in advance, so that gets paid
+        self.bank.transfer_to(self.ex_rent, 1000)
+        # Now we are even more overdrawn
+        self.assertEqual(self.bank.balance(), -70 + -1000)
+        self.assertEqual(self.ex_rent.balance(), 1000)
+
+        # Create two statement lines and assign each to the housemate income account
+        line1, line2 = self.create_incoming_rent_payments(620, 620)
+        line1.create_transaction(self.inc_housemate)
+        line2.create_transaction(self.inc_housemate)
+
+        # Create transaction for housemate1's payment
+        with db_transaction.atomic():
+            transaction = Transaction.objects.create(date='2016-01-31')
+            Leg.objects.create(transaction=transaction, account=self.inc_housemate, amount=-620)
+            Leg.objects.create(transaction=transaction, account=self.ex_rent, amount=500)
+            Leg.objects.create(transaction=transaction, account=self.ex_elec, amount=20)
+            Leg.objects.create(transaction=transaction, account=self.ex_rates, amount=30)
+            Leg.objects.create(transaction=transaction, account=self.ex_food, amount=70)
+
+            line1.transaction = transaction
+            line1.save()
+
+        # Create transaction for housemate2's payment
+        with db_transaction.atomic():
+            transaction = Transaction.objects.create(date='2016-01-31')
+            Leg.objects.create(transaction=transaction, account=self.inc_housemate, amount=-620)
+            Leg.objects.create(transaction=transaction, account=self.ex_rent, amount=500)
+            Leg.objects.create(transaction=transaction, account=self.ex_elec, amount=20)
+            Leg.objects.create(transaction=transaction, account=self.ex_rates, amount=30)
+            Leg.objects.create(transaction=transaction, account=self.ex_food, amount=70)
+
+            line2.transaction = transaction
+            line2.save()
+
+        # We should have a lot more money in the bank now
+        self.assertEqual(self.bank.balance(), 1240 - 1000 - 70)  # money received - rent - food spending
+        # The housemate income account should be empty as we've dispersed it to the relevant
+        # expense accounts
+        self.assertEqual(self.inc_housemate.balance(), 0)
+        self.assertEqual(self.ex_rent.balance(), 0)  # Rent had already been paid out, some incoming money has cancelled it out
+        # Each expense account is negative (i.e. we're waiting for expenses to come in)
+        self.assertEqual(self.ex_elec.balance(), -120 / 3)  # 120 per 3 months
+        self.assertEqual(self.ex_rates.balance(), -180 / 3)  # 180 per 3 months
+        self.assertEqual(self.ex_food.balance(), -140 + 70)
+
+        # Now we're half way through the month, so we need to order food a couple more times
+        self.bank.transfer_to(self.ex_food, 35)
+        self.bank.transfer_to(self.ex_food, 35)
+        self.assertEqual(self.bank.balance(), 1240 - 1000 - 70 - 70)  # money received - rent - two batches of food spending
+        self.assertEqual(self.ex_food.balance(), 0)  # Now we've spent all our food money for the month
+
+        # Ok, it's the end of the month now, so time to do a little admin...
+
+        # Firstly, we pay rates & electricity every 3 months, so dump
+        # the contents of those expense accounts into the relevant
+        # 'Payable' accounts (as we'll need it to pay the bills when they
+        # eventually arrive)
+        self.ex_elec.transfer_to(self.lia_elec_payable, -120 / 3)
+        self.ex_rates.transfer_to(self.lia_rates_payable, -180 / 3)
+
+        # The expense accounts should now be zeroed...
+        self.assertEqual(self.ex_elec.balance(), 0)
+        self.assertEqual(self.ex_rates.balance(), 0)
+
+        # ...and the payable accounts now have a positive balance
+        # (and we'll pay the bills out of these accounts when they
+        # eventually come in)
+        self.assertEqual(self.lia_elec_payable.balance(), 120 / 3)
+        self.assertEqual(self.lia_rates_payable.balance(), 180 / 3)
+        # And we have that money sat in the bank account
+        self.assertEqual(self.bank.balance(), 120 / 3 + 180 / 3)
+
+        # See! Accounting is fun!
+
+
+
+    # def test_three_months(self):
+    #     self.test_one_month()
+    #     self.test_one_month()
+    #     self.test_one_month()
+    #
+
+
+
+
+
