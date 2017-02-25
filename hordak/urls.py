a1cf5bf26f31a11e116f5be3ba1d6a148e96e302
Adam Charnock <adam@adamcharnock.com>
2017-02-25 18:35:30 +0000

    Adding app_name to urls.py

--- hordak/urls.py
+++ hordak/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from hordak.views import accounts
 from hordak.views import transactions
 
+app_name = 'hordak'
+
 urlpatterns = [
     url(r'^transactions/create/$', transactions.TransactionCreateView.as_view(), name='transactions_create'),
     url(r'^transactions/reconcile/$', transactions.TransactionsReconcileView.as_view(), name='transactions_reconcile'),
