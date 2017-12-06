c7cd03dc6d8f3c24ffc4d91c99f8f3524f86420e
Adam Charnock <adam@adamcharnock.com>
2017-12-06 02:55:39 +0000

    Adding math template tags

--- hordak/templatetags/hordak.py
+++ hordak/templatetags/hordak.py
@@ -1,12 +1,15 @@
 from __future__ import absolute_import
 import locale
+import logging
 
 from django import template
 from django.utils.safestring import mark_safe
 from hordak.utilities.currency import Balance
 from moneyed import Money
+from decimal import Decimal
 
 register = template.Library()
+logger = logging.getLogger(__name__)
 
 
 @register.filter(name='abs')
@@ -24,7 +27,7 @@ def inv(value):
 @register.filter()
 def currency(value):
     locale.setlocale(locale.LC_ALL, 'en_GB.UTF-8')
-    if not value:
+    if value is None:
         return None
 
     if isinstance(value, Balance):
@@ -65,3 +68,55 @@ def color_currency_inv(value):
 def negative(value):
     value = value or 0
     return abs(value) * -1
+
+
+def valid_numeric(arg):
+    if isinstance(arg, (int, float, Decimal)):
+        return arg
+    try:
+        return int(arg)
+    except ValueError:
+        return float(arg)
+
+
+# Pulled from django-mathfilters
+
+
+def handle_float_decimal_combinations(value, arg, operation):
+    if isinstance(value, float) and isinstance(arg, Decimal):
+        logger.warning('Unsafe operation: {0!r} {1} {2!r}.'.format(value, operation, arg))
+        value = Decimal(str(value))
+    if isinstance(value, Decimal) and isinstance(arg, float):
+        logger.warning('Unsafe operation: {0!r} {1} {2!r}.'.format(value, operation, arg))
+        arg = Decimal(str(arg))
+    return value, arg
+
+
+@register.filter
+def sub(value, arg):
+    """Subtract the arg from the value."""
+    try:
+        nvalue, narg = handle_float_decimal_combinations(
+            valid_numeric(value), valid_numeric(arg), '-')
+        return nvalue - narg
+    except (ValueError, TypeError):
+        try:
+            return value - arg
+        except Exception:
+            return ''
+sub.is_safe = False
+
+
+@register.filter(name='addition')
+def addition(value, arg):
+    """Float-friendly replacement for Django's built-in `add` filter."""
+    try:
+        nvalue, narg = handle_float_decimal_combinations(
+            valid_numeric(value), valid_numeric(arg), '+')
+        return nvalue + narg
+    except (ValueError, TypeError):
+        try:
+            return value + arg
+        except Exception:
+            return ''
+addition.is_safe = False
