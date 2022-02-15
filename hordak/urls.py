6e17d93273f65f93bd00d0f84dd5bb8e9aaca9aa
Petr Dlouhý <petr.dlouhy@email.cz>
2022-02-15 16:12:43 +0100

    add github actions testing

--- hordak/urls.py
+++ hordak/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 
 from hordak.views import accounts, statement_csv_import
 from hordak.views import transactions
@@ -6,64 +6,64 @@ from hordak.views import transactions
 app_name = "hordak"
 
 urlpatterns = [
-    url(
-        r"^transactions/create/$",
+    path(
+        "transactions/create/",
         transactions.TransactionCreateView.as_view(),
         name="transactions_create",
     ),
-    url(
-        r"^transactions/(?P<uuid>.+)/delete/$",
+    path(
+        "transactions/<str:uuid>/delete/",
         transactions.TransactionDeleteView.as_view(),
         name="transactions_delete",
     ),
-    url(
-        r"^transactions/currency/$", transactions.CurrencyTradeView.as_view(), name="currency_trade"
+    path(
+        "transactions/currency/", transactions.CurrencyTradeView.as_view(), name="currency_trade"
     ),
-    url(
-        r"^transactions/reconcile/$",
+    path(
+        "transactions/reconcile/",
         transactions.TransactionsReconcileView.as_view(),
         name="transactions_reconcile",
     ),
-    url(
-        r"^transactions/list/$",
+    path(
+        "transactions/list/",
         transactions.TransactionsListView.as_view(),
         name="transactions_list",
     ),
-    url(r"^transactions/legs/$", transactions.LegsListView.as_view(), name="legs_list"),
-    url(
-        r"^statement-line/(?P<uuid>.+)/unreconcile/$",
+    path("transactions/legs/", transactions.LegsListView.as_view(), name="legs_list"),
+    path(
+        "statement-line/<str:uuid>/unreconcile/",
         transactions.UnreconcileView.as_view(),
         name="transactions_unreconcile",
     ),
-    url(r"^$", accounts.AccountListView.as_view(), name="accounts_list"),
-    url(r"^accounts/create/$", accounts.AccountCreateView.as_view(), name="accounts_create"),
-    url(
-        r"^accounts/update/(?P<uuid>.+)/$",
+    path("", accounts.AccountListView.as_view(), name="accounts_list"),
+    path("accounts/create/", accounts.AccountCreateView.as_view(), name="accounts_create"),
+    path(
+        "accounts/update/<str:uuid>/",
         accounts.AccountUpdateView.as_view(),
         name="accounts_update",
     ),
-    url(
-        r"^accounts/(?P<uuid>.+)/$",
+    path(
+        "accounts/<str:uuid>/",
         accounts.AccountTransactionsView.as_view(),
         name="accounts_transactions",
     ),
-    url(r"^import/$", statement_csv_import.CreateImportView.as_view(), name="import_create"),
-    url(
-        r"^import/(?P<uuid>.*)/setup/$",
+    path("import/", statement_csv_import.CreateImportView.as_view(), name="import_create"),
+    path(
+        "import/<str:uuid>/setup/",
         statement_csv_import.SetupImportView.as_view(),
         name="import_setup",
     ),
-    url(
-        r"^import/(?P<uuid>.*)/dry-run/$",
+    path(
+        "import/<str:uuid>/dry-run/",
         statement_csv_import.DryRunImportView.as_view(),
         name="import_dry_run",
     ),
-    url(
-        r"^import/(?P<uuid>.*)/run/$",
+    path(
+        "import/<str:uuid>/run/",
         statement_csv_import.ExecuteImportView.as_view(),
         name="import_execute",
     ),
 ]
 
 # Also add in the authentication views that we need to login/logout etc
-urlpatterns += [url(r"^auth/", include("django.contrib.auth.urls"))]
+urlpatterns += [path("auth/", include("django.contrib.auth.urls"))]
