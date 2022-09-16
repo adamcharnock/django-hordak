a0cc94482e1322b10bdcd52ead3888342af6039c
Petr Dlouhý <petr.dlouhy@email.cz>
2022-09-16 18:59:09 +0200

    configure .coveragerc to show also files that were not hit at all

--- example_project/settings.py
+++ example_project/settings.py
@@ -69,7 +69,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ]
+            ],
+            "debug": DEBUG,
         },
     }
 ]
