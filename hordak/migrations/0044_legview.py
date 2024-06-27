eb161a2111b034e33d8b955c1c2f9844f0f5fec4
Adam Charnock <adam@adamcharnock.com>
2024-06-27 05:54:01 +0200

    Renaming migration file to be more descriptive, #120

--- hordak/migrations/0044_legview.py
+++ hordak/migrations/0044_legview.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("hordak", "0043_auto_20240626_1149"),
+        ("hordak", "0043_hordak_leg_view"),
     ]
 
     operations = [
