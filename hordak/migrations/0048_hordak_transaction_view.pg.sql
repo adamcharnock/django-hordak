1e721d8925cb32672d5d245b68b48a59d095b02b
Adam Charnock <adam@adamcharnock.com>
2024-06-30 09:46:56 +0200

    Work on returns account_ids with each transaction

--- hordak/migrations/0048_hordak_transaction_view.pg.sql
+++ hordak/migrations/0048_hordak_transaction_view.pg.sql
@@ -1,6 +1,8 @@
 ------
 create view hordak_transaction_view AS (SELECT
     T.*
+    ,JSON_AGG(LA.credit_account_id) as credit_account_ids
+    ,JSON_AGG(LA.debit_account_id) as debit_account_ids
     ,jsonb_agg(jsonb_build_object('amount', L.amount, 'currency', L.currency)) as balance
 FROM
     hordak_transaction T
@@ -10,6 +12,14 @@ INNER JOIN LATERAL (
     WHERE L.transaction_id = T.id AND L.amount > 0
     GROUP BY amount_currency
     ) L ON True
+INNER JOIN LATERAL (
+    SELECT
+        (CASE WHEN L.amount > 0 THEN account_id END) AS credit_account_id,
+        (CASE WHEN NOT (L.amount > 0) THEN account_id END) AS debit_account_id
+    FROM hordak_leg L
+    WHERE L.transaction_id = T.id
+    GROUP BY account_id, L.amount > 0
+    ) LA ON True
 GROUP BY T.id, T.uuid, T.timestamp, T.date, T.description
 ORDER BY T.id DESC);
 --- reverse:
