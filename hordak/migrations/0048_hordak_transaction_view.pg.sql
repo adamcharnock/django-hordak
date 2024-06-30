535bc37f9eddb099db48b07e78fc6fd0f34f5f38
Adam Charnock <adam@adamcharnock.com>
2024-06-30 09:50:04 +0200

    Work on returns account_ids with each transaction

--- hordak/migrations/0048_hordak_transaction_view.pg.sql
+++ hordak/migrations/0048_hordak_transaction_view.pg.sql
@@ -1,8 +1,8 @@
 ------
 create view hordak_transaction_view AS (SELECT
     T.*
-    ,JSON_AGG(LA.credit_account_id) as credit_account_ids
-    ,JSON_AGG(LA.debit_account_id) as debit_account_ids
+    ,JSON_AGG(L_CR.credit_account_id) as credit_account_ids
+    ,JSON_AGG(L_DR.debit_account_id) as debit_account_ids
     ,jsonb_agg(jsonb_build_object('amount', L.amount, 'currency', L.currency)) as balance
 FROM
     hordak_transaction T
@@ -14,12 +14,18 @@ INNER JOIN LATERAL (
     ) L ON True
 INNER JOIN LATERAL (
     SELECT
-        (CASE WHEN L.amount > 0 THEN account_id END) AS credit_account_id,
-        (CASE WHEN NOT (L.amount > 0) THEN account_id END) AS debit_account_id
-    FROM hordak_leg L
-    WHERE L.transaction_id = T.id
-    GROUP BY account_id, L.amount > 0
-    ) LA ON True
+        account_id AS credit_account_id
+    FROM hordak_leg
+    WHERE transaction_id = T.id AND amount > 0
+    GROUP BY account_id
+    ) L_CR ON True
+INNER JOIN LATERAL (
+    SELECT
+        account_id AS debit_account_id
+    FROM hordak_leg
+    WHERE transaction_id = T.id AND amount < 0
+    GROUP BY account_id
+    ) L_DR ON True
 GROUP BY T.id, T.uuid, T.timestamp, T.date, T.description
 ORDER BY T.id DESC);
 --- reverse:
